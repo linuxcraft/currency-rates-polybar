0349acde6f71e4d1405861cf482735f0163caac8
Peskov Sergey <serpesgeikov@gmail.com>
2022-01-26 17:13:44 +0600

    docs and comments

--- scripts/crypto.py
+++ scripts/crypto.py
@@ -5,28 +5,36 @@ import requests
 from decimal import Decimal
 
 
-def getCrytpo(name, icon):
+def getCrytpo(cryptoName, icon, baseCurrency='usd'):
     '''Parses json response and outputs value to polybar'''
     json = requests.get(
-            f'https://api.coingecko.com/api/v3/coins/{name}',
+            f'https://api.coingecko.com/api/v3/coins/{cryptoName}',
             ).json()["market_data"]
-    local_price = round(Decimal(json["current_price"]['usd']))
+    local_price = round(Decimal(json["current_price"][f'{baseCurrency}']))
     sys.stdout.write(f'{icon}{local_price} ')
 
 
 '''
-Icons:
-    fonts: cryptocoins
-    bitcoin:     
-    ethereum:    
-    monero: 
+getCrypto() takes 3 parameters:
+- The first is the name of the cryptocurrency.
+    You can find the list of names here: https://api.coingecko.com/api/v3/coins/
 
-    fonts: material design icons
-    bitcoin-icons: 󰠓
-    ethereum-icons: 󰡪
+- The second is the icon that will be displayed in the polybar
+    (use spaces to adjust the display)
+    Examples of icons:
+        fonts: cryptocoins
+        bitcoin:     
+        ethereum:    
+        monero: 
+
+        fonts: material design icons
+        bitcoin-icons: 󰠓
+        ethereum-icons: 󰡪
+
+- The third is an optional parameter,
+    it's necessary to get the price in another currency (default usd)
 '''
 
-
-getCrytpo('bitcoin', '󰠓 ')
+getCrytpo('bitcoin', ' ')
 getCrytpo('ethereum', ' ')
 getCrytpo('monero', ' ')
--- scripts/fiat.py
+++ scripts/fiat.py
@@ -6,13 +6,23 @@ from decimal import Decimal
 
 
 def getFiat(currency, baseCurrency='USD'):
-    '''Parses json response and outputs value to polybar'''
+    '''Parses json response and return price'''
     return round(Decimal(requests.get(
         f'https://freecurrencyrates.com/api/action.php?do=cvals&\
                 iso={currency}&f={baseCurrency}&v=1&s=cbr',
         ).json()[f'{currency}']), 2)
 
 
+'''
+getFiat() takes 2 parameters:
+- The first is the name of the currency.
+
+- The second is an optional parameter,
+    it's necessary to get the price in another currency (default USD)
+'''
+
+# sys.stdout.write() will output the content to the polybar.
+# Use as default print() (and use spaces to adjust the display)
 sys.stdout.write(f'󰆱{getFiat("RUB")} ')
 sys.stdout.write(f'󰡥{getFiat("KZT")}({getFiat("KZT", "RUB")}) ')
 sys.stdout.write(f'󰞼{getFiat("JPY")} ')
